303137965f9ad818eab507cf280d4dd6a803f04f
Luc Van Keer <luc.vankeer@telenet.be>
2024-02-06 09:05:49 +0100

    Added: tailwindcss+alpine

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./resources/views/**/*.antlers.html",
+    "./**/*.md",
+    "./**/*.yaml",
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+}
+
